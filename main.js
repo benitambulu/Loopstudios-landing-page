ca9d9e68f0581e39b376fc903bcbae49b88aebb4
Benita Mbulu <beniottabenita@gmail.com>
2025-01-09 00:53:21 +0100

    modified index.html and main.js

--- main.js
+++ main.js
@@ -15,6 +15,6 @@ menuIcon.addEventListener('click', () => {
     <path d="M17.778.808l1.414 1.414L11.414 10l7.778 7.778-1.414 1.414L10 11.414l-7.778 7.778-1.414-1.414L8.586 10 .808 2.222 2.222.808 10 8.586 17.778.808z" 
           fill="#FFF" />
 </svg>` // X Icon
-      : `<svg class="menu-icon" width="24" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="#FFF" fill-rule="evenodd">
+      : `<svg class="menu-icon" width="30" height="24" xmlns="http://www.w3.org/2000/svg"><g fill="#FFF" fill-rule="evenodd">
           <path d="M0 0h24v2H0zM0 7h24v2H0zM0 14h24v2H0z"/></g></svg>`; // Menu Icon
   });
